812cc0393b42479fae9856fe6f99e044d626226b
cuntAdnan <adnanbarwatiya32@gmail.com>
2024-07-11 00:40:21 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+from util import classify, set_background
+
+
+set_background('./bgs/bg2.jpg')
+
+
+st.markdown("<h1 style='color: black;'>Pneumonia Classification</h1>", unsafe_allow_html=True)
+
+
+
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+
+
+model = load_model('./model/pneumonia_classifier.h5')
+
+
+with open('./model/labels.txt', 'r') as f:
+    class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
+    f.close()
+
+
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width=True)
+
+    
+    class_name, conf_score = classify(image, model, class_names)
+
+    
+    st.write("## {}".format(class_name))
+    st.write("### score: {}%".format(int(conf_score * 1000) / 10))
